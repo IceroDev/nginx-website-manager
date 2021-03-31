a050b480065c9aef261acb1958219538f287052f
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-03-31 14:54:57 +0200

    Update nginx-website-manager.sh

--- nginx-website-manager.sh
+++ nginx-website-manager.sh
@@ -35,7 +35,6 @@ if [ $qvvf == "1" ]; then
         sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
         echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
         sudo apt update
-        sudo apt -y install php7.4
         sudo apt -y install php7.4-fpm
     fi
     echo "Php est installé."
